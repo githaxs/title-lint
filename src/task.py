f16b070e9a4c6c0ab5b8c2de59c15812c3fe0638
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2021-04-02 17:14:37 -0400

    Refactor to use task interface

--- src/task.py
+++ src/task.py
@@ -1,18 +1,26 @@
 import re
 
-from base_task import BaseTask
+from task_interfaces import MetaTaskInterface
 
 
-class Task(BaseTask):
+class Task(MetaTaskInterface):
     """
     Verifies the Title of a Pull Request matches a provided regex.
     """
 
-    def _execute(self, github_body) -> bool:
-        if self.settings is None or self.settings.get("regex") is None:
+    name = "Title Lint"
+    slug = "title-lint"
+    pass_summary = "Your Pull Request Title passes the check."
+    pass_text = ""
+    fail_text = ""
+    actions = None
+
+    def _execute(self, github_body, settings) -> bool:
+        if settings is None or settings.get("regex") is None:
+            self.fail_message = "Could not find valid settings for Title Lint. Please refer to the [documentation](https://github.com/githaxs/title-lint#installation) and ensure you have configured it correctly."
             return False
 
-        self.regex = self.settings["regex"]
+        self.regex = settings["regex"]
         check_regex = re.compile(self.regex)
 
         return bool(
@@ -22,23 +30,6 @@ class Task(BaseTask):
     def _requested_action(self):
         pass
 
-    def _get_task_name(self):
-        return "Title Lint"
-
-    def _get_task_slug(self) -> str:
-        return "title-lint"
-
-    def _get_fail_summary(self) -> str:
+    @property
+    def fail_summary(self) -> str:
         return "Pull Request Title must match regex: %s" % self.regex
-
-    def _get_fail_text(self) -> str:
-        return ""
-
-    def _get_pass_summary(self) -> str:
-        return ":+1:"
-
-    def _get_pass_text(self) -> str:
-        return ""
-
-    def _get_actions(self):
-        return None
