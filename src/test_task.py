959b84b53c987b42849c87587d302a598ef1c1fb
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2021-04-02 21:54:36 -0400

    update tests

--- src/test_task.py
+++ src/test_task.py
@@ -4,19 +4,19 @@ from .task import Task
 def test_bad_title_check():
     task = Task()
     settings = {"regex": "fix: .*"}
-    assert task._execute({"pull_request": {"title": " bad bug"}}, settings) is False
+    assert task.execute({"pull_request": {"title": " bad bug"}}, settings) is False
 
 
 def test_good_title_check():
     task = Task()
     settings = {"regex": "fix: .*"}
-    assert task._execute({"pull_request": {"title": "fix: bad bug"}}, settings) is True
+    assert task.execute({"pull_request": {"title": "fix: bad bug"}}, settings) is True
 
 
 def test_no_settings():
     task = Task()
 
-    assert task._execute({}, None) is False
+    assert task.execute({}, None) is False
 
 
 def test_malformed_body():
@@ -24,4 +24,4 @@ def test_malformed_body():
 
     settings = {"regex": "fix: .*"}
 
-    assert task._execute({}, settings) is False
+    assert task.execute({}, settings) is False
